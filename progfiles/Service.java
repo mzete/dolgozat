6b02bdfc943c5e3657e31fa40847a3e651d60683
ZETE\Zete-Örs <martonzete@gmail.com>
2016-06-13 07:54:47 +0300

    upnp, irányitás pid problema, javitások, uml

---
+++ progfiles/Service.java
@@ -0,0 +1,31 @@
+import org.fourthline.cling.binding.annotations.*;
+
+@UpnpService(
+        serviceId = @UpnpServiceId("MyService"),
+        serviceType = @UpnpServiceType(value = "MyServie", version = 1)
+)
+public class Service {
+
+    @UpnpStateVariable(defaultValue = "0", sendEvents = false)
+    private boolean target = false;
+
+    @UpnpStateVariable(defaultValue = "0")
+    private boolean status = false;
+
+    @UpnpAction
+    public void setTarget(@UpnpInputArgument(name = "NewTargetValue")
+                          boolean newTargetValue) {
+        target = newTargetValue;
+        status = newTargetValue;
+    }
+
+    @UpnpAction(out = @UpnpOutputArgument(name = "RetTargetValue"))
+    public boolean getTarget() {
+        return target;
+    }
+
+    @UpnpAction(out = @UpnpOutputArgument(name = "ResultStatus"))
+    public boolean getStatus() {
+        return status;
+    }
+}
